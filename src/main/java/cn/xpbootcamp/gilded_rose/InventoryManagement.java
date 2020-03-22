245b72a60d6e37307efbb7ac103ff25aa96eee47
hang.liu <hang.liu@thoughtworks.com>
2020-03-22 18:56:30 +0800

    feat: should change double rate spend more then sellIn

--- src/main/java/cn/xpbootcamp/gilded_rose/InventoryManagement.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/InventoryManagement.java
@@ -2,6 +2,14 @@ package cn.xpbootcamp.gilded_rose;
 
 public class InventoryManagement {
     public int calculateQualityAfterDays(String type, int sellIn, int quality, int qualityRate,int days) {
-        return quality;
+        int currentQuality = quality;
+        if (sellIn >= days) {
+            currentQuality += days * qualityRate;
+        } else {
+            currentQuality += sellIn * qualityRate + (days - sellIn) * 2 * qualityRate;
+        }
+
+
+        return currentQuality;
     }
 }
--- src/test/java/cn/xpbootcamp/gilded_rose/InventoryManagementTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/InventoryManagementTest.java
@@ -6,11 +6,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 
 class InventoryManagementTest {
+    private InventoryManagement inventoryManagement = new InventoryManagement();
+
     @Test
     void should_return_same_when_spend_0_day(){
-        InventoryManagement inventoryManagement = new InventoryManagement();
         int quality = 2;
         assertEquals(quality, inventoryManagement.calculateQualityAfterDays(anyString(), anyInt(), quality, anyInt(), 0));
+    }
+
+    @Test
+    void should_change_double_rate_spend_more_than_sellIn(){
+        int sellIn = 2;
+        int quality = 10;
+        int qualityRate = -1;
+        int days = 5;
+        int expectedQuality = 2;
+        assertEquals(expectedQuality, inventoryManagement.calculateQualityAfterDays(anyString(), sellIn, quality, qualityRate, days));
     }
 
 }
